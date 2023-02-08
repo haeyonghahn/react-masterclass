fecf6ccaf3f622c6c71fa91a396bed4ba0bfb45e
한해용 <yong7317>
2023-02-08 18:02:53 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import styled from "styled-components";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hi</h1>
+    <div style={{ display: "flex" }}>
+      <div style={{ backgroundColor: "tomato", width: 100, height: 100 }}></div>
+      <div style={{ backgroundColor: "tomato", width: 100, height: 100 }}></div>
     </div>
   );
 }
