af5deac3cb5b4956bd3d1cbefb3c31120ef28a6c
한해용 <yong7317>
2023-02-09 14:15:19 +0900

    Theme

--- src/App.js
+++ src/App.js
@@ -2,51 +2,21 @@ import styled, { keyframes } from "styled-components";
 
 const Wrapper = styled.div`
   display: flex;
-`;
-
-const animation = keyframes`
-  0% {
-    transform: rotate(0deg);
-    border-radius: 0px;
-  } 
-  50% {
-    border-radius: 100px;
-  }
-  100% {
-    transform: rotate(360deg);
-    border-radius: 0px;
-  }
-`;
-
-const Emoji = styled.span`
-  font-size: 36px;
-`;
-
-const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color: tomato;
-  display: flex;
+  height: 100vh;
+  width: 100vw;
   justify-content: center;
   align-items: center;
-  animation: ${animation} 1s linear infinite;
-  ${Emoji} {
-    &:hover {
-      font-size: 98px;
-    }
-    &:active {
-      opacity: 0;
-    }
-  }
+  background-color: ${(props) => props.theme.backgroundColor};
+`;
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <Emoji as="p">😁</Emoji>
-      </Box>
-      <Emoji>😒</Emoji>
+      <Title>Hello</Title>
     </Wrapper>
   );
 }
