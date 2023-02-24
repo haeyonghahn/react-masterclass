998c84f9e557705caf40d39759806bd6d14ed230
한해용 <yong7317>
2023-02-24 18:08:25 +0900

    Final Touches

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,4 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
+import { Helmet } from "react-helmet";
 import {
   Link,
   Route,
@@ -156,11 +157,15 @@ function Coin() {
   );
   const { isLoading: tickersLoading, data: tickersData } = useQuery<PriceData>(
     ["tickers", coinId],
-    () => fetchCoinTickers(coinId)
+    () => fetchCoinTickers(coinId),
+    {
+      refetchInterval: 5000, //5초
+    }
   );
   const loading = infoLoading || tickersLoading;
   return (
     <Container>
+      <Helmet></Helmet>
       <Header>
         <Title>
           {state?.name ? state.name || "Loading..." : infoData?.name}
@@ -180,8 +185,8 @@ function Coin() {
               <span>${infoData?.symbol}</span>
             </OverviewItem>
             <OverviewItem>
-              <span>Open Source:</span>
-              <span>{infoData?.open_source ? "Yes" : "No"}</span>
+              <span>Price:</span>
+              <span>${tickersData?.quotes.USD.price.toFixed(3)}</span>
             </OverviewItem>
           </Overview>
           <Description>{infoData?.description}</Description>
