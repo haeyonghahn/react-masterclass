09db540e3e6e1165c14af9dccf74e9e6582e34ed
한해용 <yong7317>
2023-02-27 11:57:14 +0900

    Dark Mode part One

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,7 @@
 import { QueryClientProvider } from "@tanstack/react-query";
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient } from "@tanstack/react-query";
-import { ThemeProvider } from "styled-components";
 import App from "./App";
-import { theme } from "./theme";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -15,9 +12,7 @@ const queryClient = new QueryClient();
 root.render(
   // <React.StrictMode>
   <QueryClientProvider client={queryClient}>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <App />
   </QueryClientProvider>
   // </React.StrictMode>
 );
